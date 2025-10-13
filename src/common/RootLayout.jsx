d817b0ff01783e3d77fa1abc3775e9f500b9f90b
choeunbin03 <choeunbiin03@naver.com>
2025-10-13 10:30:20 +0900

    fix: 스크롤 위치 리셋 구현

--- src/common/RootLayout.jsx
+++ src/common/RootLayout.jsx
@@ -2,10 +2,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import ScrollToTop from './ScrollToTop';
 
 export default function RootLayout() {
   return (
     <div className="min-h-screen">
+      <ScrollToTop />
       <Header />
       <Sidebar />
       {/* 메인 콘텐츠: 데스크톱에서는 우측 사이드바 공간 확보, 모바일에서는 전체 너비 */}
